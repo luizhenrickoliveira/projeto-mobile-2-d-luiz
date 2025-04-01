f9cb8ba2a82cc3803c62f16c432ce67702ea9f06
luizhenrickoliveira <luiz.oliveira.matos@escola.pr.gov.br>
2025-04-01 08:02:38 -0300

    Primeiro commit

---
+++ mobile/app/(tabs)/index.tsx
@@ -0,0 +1,67 @@
+import { Image, StyleSheet, Platform } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/nubank-ultravioleta-cartao-de-credito-capa.webp')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Seja bem-vindo ao seu banco NuBank!</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Saldo: 102,934.00</ThemedText>
+        <ThemedText>
+        <ThemedText type="defaultSemiBold">
+            {Platform.select({
+            })}
+          </ThemedText>{' '}
+        
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Pix</ThemedText>
+        <ThemedText>
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Caixinha</ThemedText>
+        <ThemedText>
+          {' '}
+          <ThemedText type="defaultSemiBold"></ThemedText> {' '}
+          <ThemedText type="defaultSemiBold"></ThemedText>{' '}
+          <ThemedText type="defaultSemiBold"></ThemedText> {' '}
+          <ThemedText type="defaultSemiBold"></ThemedText>.
+        </ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 250,
+    width: 580,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
